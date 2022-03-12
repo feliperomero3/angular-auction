3c46e765800c69a7dd07637c6b940b18e6f45f6e
Felipe Romero <feliperomero3@outlook.com>
2022-03-11 21:34:02 -0600

    Generating components for ngAuction - Footer component.

--- ngAuction/src/app/app.module.ts
+++ ngAuction/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CarouselComponent
+    CarouselComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
