375e69c6925f71d0e0730f0fcb5eda80eb0d43dd
Felipe Romero <feliperomero3@outlook.com>
2022-03-14 23:23:57 -0600

    Add app routing module test.

---
+++ ngAuction/src/app/app-routing.module.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed, waitForAsync } from "@angular/core/testing";
+import { AppRoutingModule } from "./app-routing.module";
+
+describe('AppRoutingModule', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AppRoutingModule]
+    });
+  });
+
+  it('initializes', () => {
+    const module = TestBed.inject(AppRoutingModule);
+    expect(module).toBeTruthy();
+  });
+
+});
