47656d563111bb46ef745a7e83806b8e83db068f
Felipe Romero <feliperomero3@outlook.com>
2022-03-14 20:46:15 -0600

    Add app component test.

---
+++ ngAuction/src/app/app.component.spec.ts
@@ -0,0 +1,37 @@
+import { Component } from "@angular/core";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>
+
+  @Component({ selector: 'nga-search', template: '' })
+  class SearchStubComponent { }
+
+  @Component({ selector: 'nga-footer', template: '' })
+  class FooterStubComponent { }
+
+  @Component({ selector: 'nga-navbar', template: '' })
+  class NavbarStubComponent { }
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [
+        AppComponent,
+        SearchStubComponent,
+        FooterStubComponent,
+        NavbarStubComponent
+      ]
+    }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+  }));
+
+
+  it('should create the app', () => {
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+})
