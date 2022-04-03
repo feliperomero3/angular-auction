c85fb87f028fc120b7e66e0a537c5398356b0956
Felipe Romero <feliperomero3@outlook.com>
2022-04-02 22:18:01 -0600

    Update the product service.

--- ngAuction/src/app/shared/product.service.ts
+++ ngAuction/src/app/shared/product.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  getProducts(): Product[] {
+    return products.map(p => new Product(p.id, p.title,
+      p.price, p.rating, p.description, p.categories));
+  }
+
+  getProductById(productId: number): Product | undefined {
+    return products.find(p => p.id === productId);
+  }
 }
