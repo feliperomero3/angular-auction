cb9324b3787aff4f0a01b5a7861cd7ff10e45c85
Felipe Romero <feliperomero3@outlook.com>
2022-04-03 23:05:15 -0500

    Update home component.

--- ngAuction/src/app/home/home.component.ts
+++ ngAuction/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../shared/product';
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'nga-home',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private productService: ProductService) {
+
+  }
 
   ngOnInit(): void {
+    this.products = this.getProducts();
+  }
+
+  private getProducts(): Product[] {
+    return this.productService.getProducts();
   }
 
 }
